8fa2dbb7c670bee1dac9163e36a2620854eca0eb
hansraj <hansubabagmaing@gmail.com>
2023-10-11 13:36:17 +0530

    other screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task_app/homepage.dart';
+import 'package:task_app/login_screen.dart';
+import 'package:task_app/verification_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: VerficationScreen(),
     );
   }
 }
